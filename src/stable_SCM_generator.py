cab1a1f954351628fb6b6d9d6cac9e46aa9238c0
Jake Nichol <jefnich@sandia.gov>
2024-07-19 14:45:49 -0600

    Update documentation and reformat

--- src/stable_SCM_generator.py
+++ src/stable_SCM_generator.py
@@ -1,3 +1,62 @@
+"""
+This script provides functions to generate stable Structural Causal Models (SCMs) for spatiotemporal datasets.
+It includes utilities for creating random positive-semidefinite matrices, padding matrices, generating coefficient matrices,
+checking matrix stability, and generating spatiotemporal datasets. Additionally, it provides functions to convert coefficient matrices
+to graph structures compatible with the TIGRAMITE library and the CausalNex library.
+
+Dependencies:
+- numpy
+- scipy
+- causalnex
+- tigramite
+
+Functions:
+----------
+1. multivariate_gaussian(pos, mu, Sigma)
+    Returns the multivariate Gaussian distribution on array pos.
+
+2. get_random_positive_semidefinite_matrix(size, Sigma)
+    Generates a random positive-semidefinite matrix with given standard deviation.
+
+3. pad_n_cols_right_of_2d_matrix(arr, n, fill_value)
+    Adds n columns of zeros to the right of a 2D numpy array matrix.
+
+4. pad_n_rows_below_2d_matrix(arr, n, fill_value)
+    Adds n rows of zeros below a 2D numpy array matrix.
+
+5. create_coefficient_matrix(local_coefficients, grid_size, fill_value=0.0)
+    Generates a (grid_size**2, grid_size**2) size matrix from a given (3, 3) matrix for a toroidal space.
+
+6. create_nonwrapping_coefficient_matrix(local_coefficients, grid_size=3, fill_value=0.0)
+    Generates a (grid_size**2, grid_size**2) size matrix from a given (3, 3) matrix for a non-toroidal space.
+
+7. is_stable(matrix, verbose=0)
+    Checks for numerical stability, i.e., if all eigenvalues of the given matrix are < 1.0.
+
+8. get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold=None, verbose=0)
+    Generates a random local dependence coefficient matrix that will produce a stable dynamics matrix.
+
+9. get_graph_from_coefficient_matrix(dynamics_matrix, func=lambda x: x, return_val_matrix=False)
+    Converts a dynamics matrix into a TIGRAMITE compatible graph object.
+
+10. generate_dataset(T, grid_size, spatial_coefficients=None, dependence_density=None, min_value=None, error_sigma=0.1, error_mean=0, verbose=0)
+    Generates a spatiotemporal dataset based on a given grid size and spatial coefficients.
+
+11. get_graph_from_structure_model(structure_model, include_val_matrix=True)
+    Converts a causalnex.structure.StructureModel to a string-graph and val_matrix in the style of the TIGRAMITE library.
+
+Example Usage:
+--------------
+To generate a spatiotemporal dataset:
+>>> dataset = generate_dataset(T=100, grid_size=5, dependence_density=0.5, min_value=0.1)
+
+To convert a dynamics matrix to a TIGRAMITE compatible graph:
+>>> graph = get_graph_from_coefficient_matrix(dynamics_matrix)
+
+To convert a CausalNex StructureModel to a TIGRAMITE compatible graph:
+>>> graph, val_matrix = get_graph_from_structure_model(structure_model)
+"""
+
 import numpy as np
 from numpy import linalg as LA
 import random
@@ -5,7 +64,7 @@ from causalnex.structure import StructureModel
 from scipy import stats
 from scipy.sparse import random as rndm
 from tigramite.toymodels import structural_causal_processes
-from typing import Union, Callable
+from typing import Union
 
 
 def multivariate_gaussian(pos, mu, Sigma):
@@ -181,7 +240,9 @@ def create_coefficient_matrix(local_coefficients, grid_size, fill_value=0.0):
     grid = np.ones((grid_size, grid_size))
 
     # Create a coefficient matrix with dimensions (grid_size, grid_size, grid_size, grid_size) filled with fill_value
-    coefficient_matrix = np.full((grid_size, grid_size, grid_size, grid_size), fill_value)
+    coefficient_matrix = np.full(
+        (grid_size, grid_size, grid_size, grid_size), fill_value
+    )
     # Reshape the local_coefficients to (3, 3) and make a copy
     coefficients = np.array(local_coefficients)
     coefficients = coefficients.reshape(3, 3).copy()
@@ -202,15 +263,21 @@ def create_coefficient_matrix(local_coefficients, grid_size, fill_value=0.0):
                     # Calculate the offset between the current row and column indices
                     offset = (C_rowrow - C_colrow, C_rowcol - C_colcol)
                     # Assign the corresponding value from the padded coefficients matrix to the coefficient_matrix
-                    coefficient_matrix[C_rowrow, C_rowcol, C_colrow, C_colcol] = padded_coeff[
-                        (padded_coeff_center[0] - offset[0]) % padded_coeff.shape[0],
-                        (padded_coeff_center[1] - offset[1]) % padded_coeff.shape[1],
-                    ]
+                    coefficient_matrix[C_rowrow, C_rowcol, C_colrow, C_colcol] = (
+                        padded_coeff[
+                            (padded_coeff_center[0] - offset[0])
+                            % padded_coeff.shape[0],
+                            (padded_coeff_center[1] - offset[1])
+                            % padded_coeff.shape[1],
+                        ]
+                    )
 
     return coefficient_matrix.reshape(grid_size**2, grid_size**2)
 
 
-def create_nonwrapping_coefficient_matrix(local_coefficients, grid_size=3, fill_value=0.0):
+def create_nonwrapping_coefficient_matrix(
+    local_coefficients, grid_size=3, fill_value=0.0
+):
     """A mapping function that generates a (grid_size**2, grid_size**2) size matrix from a given (3, 3) matrix.
 
     The given (3, 3) matrix is a local dependence coefficient matrix that determines the dependence of
@@ -236,7 +303,9 @@ def create_nonwrapping_coefficient_matrix(local_coefficients, grid_size=3, fill_
     # Create a grid of ones with dimensions (grid_size, grid_size)
     grid = np.ones((grid_size, grid_size))
     # Create a coefficient matrix with dimensions (grid_size, grid_size, grid_size, grid_size) filled with fill_value
-    coefficient_matrix = np.full((grid_size, grid_size, grid_size, grid_size), fill_value)
+    coefficient_matrix = np.full(
+        (grid_size, grid_size, grid_size, grid_size), fill_value
+    )
     # Reshape the local_coefficients to (3, 3) and make a copy
     coefficients = np.array(local_coefficients)
     coefficients = coefficients.reshape(3, 3).copy()
@@ -270,10 +339,12 @@ def create_nonwrapping_coefficient_matrix(local_coefficients, grid_size=3, fill_
                         continue
 
                     # Assign the corresponding value from the padded coefficients matrix to the coefficient_matrix
-                    coefficient_matrix[C_rowrow, C_rowcol, C_colrow, C_colcol] = padded_coeff[
-                        padded_coeff_center[0] - offset[0],
-                        padded_coeff_center[1] - offset[1],
-                    ]
+                    coefficient_matrix[C_rowrow, C_rowcol, C_colrow, C_colcol] = (
+                        padded_coeff[
+                            padded_coeff_center[0] - offset[0],
+                            padded_coeff_center[1] - offset[1],
+                        ]
+                    )
 
     return coefficient_matrix.reshape(grid_size**2, grid_size**2)
 
@@ -301,7 +372,9 @@ def is_stable(matrix, verbose=0):
     return np.all(np.abs(eigenvalues) < 1.0)
 
 
-def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold=None, verbose=0):
+def get_random_stable_coefficient_matrix(
+    grid_size, density, min_value_threshold=None, verbose=0
+):
     """Generates a random local dependence coefficient matrix that will produce a stable dynamics matrix of size (grid_size**2, grid_size**2).
 
     The dynamics matrix is a mapping of the local spatial coefficient matrix onto gridded toroidal space of size
@@ -331,14 +404,18 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
         # Get a random sparse matrix from the distribution above and convert it to a large dynamics matrix
         local_coefficients = rndm(3, 3, density=density, random_state=rng, data_rvs=rvs)
         local_coefficients = local_coefficients.todense()
-        dynamics_matrix = create_coefficient_matrix(local_coefficients, grid_size, fill_value=0.0)
+        dynamics_matrix = create_coefficient_matrix(
+            local_coefficients, grid_size, fill_value=0.0
+        )
 
         try:
             if is_stable(dynamics_matrix, verbose - 1):
                 min_value_threshold
                 eigenvalues, _ = LA.eig(dynamics_matrix)
                 operator_norm = np.max(np.abs(eigenvalues))
-                min_value_actual = np.min(np.abs(local_coefficients[np.nonzero(local_coefficients)]))
+                min_value_actual = np.min(
+                    np.abs(local_coefficients[np.nonzero(local_coefficients)])
+                )
 
                 if min_value_actual >= min_value_threshold:
                     if verbose:
@@ -352,11 +429,16 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
                         )
                         print(
                             "Lowest scaled operator norm to achieve min_val_threshold = abs(min_value_threshold/min_value_actual*operator_norm) = {} <- must be less than 1.0".format(
-                                np.abs(min_value_threshold / min_value_actual * operator_norm)
+                                np.abs(
+                                    min_value_threshold
+                                    / min_value_actual
+                                    * operator_norm
+                                )
                             )
                         )
                     assert (
-                        np.abs(min_value_threshold / min_value_actual * operator_norm) < 1
+                        np.abs(min_value_threshold / min_value_actual * operator_norm)
+                        < 1
                     ), "min_value_threshold = {}, min_value_actual = {}, operator_norm = {}".format(
                         min_value_threshold, min_value_actual, operator_norm
                     )
@@ -374,11 +456,16 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
                         )
                         print(
                             "Lowest scaled operator norm to achieve min_val_threshold = abs(min_value_threshold/min_value_actual*operator_norm) = {} <- must be less than 1.0".format(
-                                np.abs(min_value_threshold / min_value_actual * operator_norm)
+                                np.abs(
+                                    min_value_threshold
+                                    / min_value_actual
+                                    * operator_norm
+                                )
                             )
                         )
                     assert (
-                        np.abs(min_value_threshold / min_value_actual * operator_norm) < 1
+                        np.abs(min_value_threshold / min_value_actual * operator_norm)
+                        < 1
                     ), "min_value_threshold = {}, min_value_actual = {},operator_norm= {}".format(
                         min_value_threshold, min_value_actual, operator_norm
                     )
@@ -389,11 +476,15 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
 
                     if verbose:
                         print(
-                            "min scaling = {}, max scaling = {}, scaling = {}".format(min_scaling, max_scaling, scaling)
+                            "min scaling = {}, max scaling = {}, scaling = {}".format(
+                                min_scaling, max_scaling, scaling
+                            )
                         )
 
                     scaled_local_coefficients = scaling * local_coefficients
-                    dynamics_matrix_ = create_coefficient_matrix(scaled_local_coefficients, grid_size, fill_value=0.0)
+                    dynamics_matrix_ = create_coefficient_matrix(
+                        scaled_local_coefficients, grid_size, fill_value=0.0
+                    )
 
                     if verbose:
                         print("scaled_N = \n{}".format(scaled_local_coefficients))
@@ -403,7 +494,13 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
 
                     if verbose:
                         min_val_met = (
-                            np.min(np.abs(scaled_local_coefficients[np.nonzero(scaled_local_coefficients)]))
+                            np.min(
+                                np.abs(
+                                    scaled_local_coefficients[
+                                        np.nonzero(scaled_local_coefficients)
+                                    ]
+                                )
+                            )
                             >= min_value_threshold
                         )
                         print(
@@ -414,10 +511,20 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
                         )
                         eigenvalues_, _ = LA.eig(dynamics_matrix_)
                         scaled_operator_norm = np.max(np.abs(eigenvalues_))
-                        print("Operator norm of scaled_N = {}".format(scaled_operator_norm))
+                        print(
+                            "Operator norm of scaled_N = {}".format(
+                                scaled_operator_norm
+                            )
+                        )
 
-                    scaling_equality = np.all(np.equal(scaled_local_coefficients, (scaling * local_coefficients)))
-                    assert scaling_equality, "local_coefficnets was not scaled correctly!"
+                    scaling_equality = np.all(
+                        np.equal(
+                            scaled_local_coefficients, (scaling * local_coefficients)
+                        )
+                    )
+                    assert (
+                        scaling_equality
+                    ), "local_coefficnets was not scaled correctly!"
                     local_coefficients = scaled_local_coefficients
                     break
                 continue
@@ -432,7 +539,9 @@ def get_random_stable_coefficient_matrix(grid_size, density, min_value_threshold
     return local_coefficients
 
 
-def get_graph_from_coefficient_matrix(dynamics_matrix, func=lambda x: x, return_val_matrix=False):
+def get_graph_from_coefficient_matrix(
+    dynamics_matrix, func=lambda x: x, return_val_matrix=False
+):
     """Get a TIGRAMITE compatible graph object from a given dynamics matrix.
 
     First converts the dynamics matrix into a structural causal mode (SCM) according to TIGRAMITE's link dict definition,
@@ -461,7 +570,10 @@ def get_graph_from_coefficient_matrix(dynamics_matrix, func=lambda x: x, return_
     SCM = {}
 
     for row in range(dynamics_matrix.shape[0]):
-        SCM[row] = [((col, -1), dynamics_matrix[row][col], func) for col in range(dynamics_matrix.shape[1])]
+        SCM[row] = [
+            ((col, -1), dynamics_matrix[row][col], func)
+            for col in range(dynamics_matrix.shape[1])
+        ]
 
     true_graph = structural_causal_processes.links_to_graph(SCM, tau_max=1)
 
@@ -490,7 +602,7 @@ def generate_dataset(
     Generates a spatiotemporal dataset based on a given grid size and spatial coefficients.
 
     This function generates a spatiotemporal dataset based on the provided parameters.
-    It initializes a data array and runs a simulation loop to generate the data by applying the spatial coefficients 
+    It initializes a data array and runs a simulation loop to generate the data by applying the spatial coefficients
         to the previous time step's data, incorporating noise.
     The generated dataset is returned as a 3-dimensional NumPy array with the shape (grid_size, grid_size, T).
     The function also supports the option to provide custom spatial coefficients or generate them randomly based on the
@@ -528,9 +640,7 @@ def generate_dataset(
     if spatial_coefficients is None:
         assert (
             dependence_density is not None
-        ) & min_value is not None, (
-            "Since no spatial_coefficients were passed, dependence_density and min_value must be passed."
-        )
+        ) & min_value is not None, "Since no spatial_coefficients were passed, dependence_density and min_value must be passed."
         spatial_coefficients = get_random_stable_coefficient_matrix(
             grid_size, dependence_density, min_value_threshold=min_value, verbose=0
         )
@@ -543,14 +653,27 @@ def generate_dataset(
         for row in range(ROWS):
             for col in range(COLS):
                 from_left = spatial_coefficients[1, 0] * data[row, col - 1, t - 1]
-                from_right = spatial_coefficients[1, 2] * data[row, (col + 1) % ROWS, t - 1]
+                from_right = (
+                    spatial_coefficients[1, 2] * data[row, (col + 1) % ROWS, t - 1]
+                )
                 from_top = spatial_coefficients[0, 1] * data[row - 1, col, t - 1]
-                from_bottom = spatial_coefficients[2, 1] * data[(row + 1) % ROWS, col, t - 1]
+                from_bottom = (
+                    spatial_coefficients[2, 1] * data[(row + 1) % ROWS, col, t - 1]
+                )
 
-                from_top_left = spatial_coefficients[0, 0] * data[row - 1, col - 1, t - 1]
-                from_top_right = spatial_coefficients[0, 2] * data[row - 1, (col + 1) % COLS, t - 1]
-                from_bot_left = spatial_coefficients[2, 0] * data[(row + 1) % ROWS, col - 1, t - 1]
-                from_bot_right = spatial_coefficients[2, 2] * data[(row + 1) % ROWS, (col + 1) % COLS, t - 1]
+                from_top_left = (
+                    spatial_coefficients[0, 0] * data[row - 1, col - 1, t - 1]
+                )
+                from_top_right = (
+                    spatial_coefficients[0, 2] * data[row - 1, (col + 1) % COLS, t - 1]
+                )
+                from_bot_left = (
+                    spatial_coefficients[2, 0] * data[(row + 1) % ROWS, col - 1, t - 1]
+                )
+                from_bot_right = (
+                    spatial_coefficients[2, 2]
+                    * data[(row + 1) % ROWS, (col + 1) % COLS, t - 1]
+                )
 
                 from_self = spatial_coefficients[1, 1] * data[row, col, t - 1]
 
@@ -570,7 +693,9 @@ def generate_dataset(
     return data
 
 
-def get_graph_from_structure_model(structure_model: StructureModel, include_val_matrix=True) -> Union[tuple, list]:
+def get_graph_from_structure_model(
+    structure_model: StructureModel, include_val_matrix=True
+) -> Union[tuple, list]:
     """
     Convert a causalnex.structure.StructureModel to a string-graph and val_matrix in the style of the Tigramite library.
 
@@ -627,7 +752,9 @@ def get_graph_from_structure_model(structure_model: StructureModel, include_val_
             lag = child_lag - parent_lag
             graph[parent_var, child_var, lag] = "-->"
             if parent_lag == 0:
-                graph[child_var, parent_var, 0] = "<--"  # <-- used because of what Tigramite does.
+                graph[child_var, parent_var, 0] = (
+                    "<--"  # <-- used because of what Tigramite does.
+                )
             if include_val_matrix:
                 val_matrix[parent_var, child_var, lag] = child_weight
                 if parent_lag == 0:
